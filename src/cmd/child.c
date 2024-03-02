97b1401150d9e63cc3f33250fd99ecbd5a4334fb
Chamber6821 <vania.osinin@yandex.ru>
2024-03-02 09:23:50 +0300

    feat: run child program from parent

---
+++ src/cmd/child.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("I am child!\n");
+  return 42;
+}
--- src/cmd/parent.c
+++ src/cmd/parent.c
@@ -1,1 +1,17 @@
-int main() {}
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  pid_t child = fork();
+  if (child == 0) {
+    char *argv[] = {NULL};
+    if (execv("build/child", argv) == -1) return errno;
+  }
+  printf("I am parent!\n");
+
+  waitpid(-1, NULL, 0);
+}
