00eecae0c58d6c3e0ac9e28ff7cb099c093c3781
Chamber6821 <vania.osinin@yandex.ru>
2024-03-03 21:29:38 +0300

    feat(parent): sort and print env variables

--- src/cmd/parent.c
+++ src/cmd/parent.c
@@ -1,19 +1,32 @@
+#define _XOPEN_SOURCE
+#define _GNU_SOURCE
 #include "error.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main() {
-  const char *childPath = getenv("CHILD_PATH");
-  if (!childPath)
-    error(CHILD_PATH_NOT_SET, "Environment variable CHILD_PATH not set");
-  pid_t child = fork();
-  if (child == 0) {
-    char *argv[] = {NULL};
-    if (execv("build/child", argv) == -1) return errno;
+void sortString(char **strings, int (*comparator)(const char *, const char *)) {
+  for (char **left = strings; *left; ++left)
+    for (char **right = left + 1; *right; ++right) {
+      if (comparator(*left, *right) > 0) {
+        char *temp = *left;
+        *left = *right;
+        *right = temp;
+      }
+    }
+}
+
+int main(int argc, char **argv, char **envp) {
+  (void)argc;
+  (void)argv;
+
+  sortString(envp, strcmp);
+  for (char **env = envp; *env; ++env) {
+    printf("%s\n", *env);
   }
   printf("I am parent!\n");
 
