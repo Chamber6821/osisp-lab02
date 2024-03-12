e56d1984d560a0465e3d9ce0159898c63c6d1c75
Chamber6821 <vania.osinin@yandex.ru>
2024-03-12 17:41:47 +0300

    feat(child): print env variables from file

--- src/cmd/child.c
+++ src/cmd/child.c
@@ -1,11 +1,40 @@
+#define _GNU_SOURCE
+#include "env.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 enum Choice { UNKNOWN_CHOICE, USE_GETENV, USE_ENVP, USE_ENVIRON, QUIT };
 
-int main(int argc, char **argv) {
+typedef const char *(*getenv_f)(const char *);
+
+getenv_f getenvByChoice(enum Choice choice) {
+  switch (choice) {
+  case USE_GETENV: return (getenv_f)getenv;
+  case USE_ENVP: return getFromMyEnviron;
+  case USE_ENVIRON: return getFromEnviron;
+  default: return NULL;
+  }
+}
+
+int main(int argc, char **argv, char **envp) {
   (void)argc;
+  const char *name = argv[0];
+  const char *filename = argv[1];
+
   enum Choice choice;
   sscanf(argv[2], "%d", (int *)&choice);
-  printf("I am %s! File is %s. Choice %d\n", argv[0], argv[1], choice);
-  return 42;
+  getenv_f myGetenv = getenvByChoice(choice);
+
+  myEnviron = envp;
+
+  printf("%s\n", name);
+
+  FILE *file = fopen(filename, "r");
+  while (!feof(file)) {
+    char var[256] = {0};
+    fscanf(file, "%s;", var);
+    if (strlen(var) == 0) continue;
+    printf("%s=%s\n", var, myGetenv(var));
+  }
+  fclose(file);
 }
